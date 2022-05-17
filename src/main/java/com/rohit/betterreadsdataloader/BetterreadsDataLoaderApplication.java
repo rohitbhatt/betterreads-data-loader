8c430d1917cde64ee104a5f15a90d1468e186316
rohit-bhatt <rohit_bhatt@apple.com>
2022-05-17 16:27:18 -0700

    Cassandra Connect

--- src/main/java/com/rohit/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/com/rohit/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -1,21 +1,39 @@
 package com.rohit.betterreadsdataloader;
 
+import com.rohit.betterreadsdataloader.author.Author;
+import com.rohit.betterreadsdataloader.author.AuthorService;
 import com.rohit.betterreadsdataloader.connection.DataStaxAstraProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
 import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
 
+    @Autowired
+    AuthorService authorService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
 	}
+
+	@PostConstruct
+	public void start(){
+	    System.out.println("Application Started");
+        Author author = new Author();
+        author.setId("A123");
+        author.setName("Rohit");
+        author.setPersonalName("Anshu");
+
+        authorService.save(author);
+    }
 
     /**
      * This is necessary to have the Spring Boot app use the Astra secure bundle
---
+++ src/main/java/com/rohit/betterreadsdataloader/author/AuthorService.java
@@ -0,0 +1,17 @@
+package com.rohit.betterreadsdataloader.author;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+    @Autowired
+    AuthorRepository authorRepository;
+
+    public Author save(Author author){
+        return authorRepository.save(author);
+    }
+}
+
+
